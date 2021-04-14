1d9dc46e175d312e01be6add0d8f1aee7fe46a3e
Serhii Hubernyi <natalyahuberna@gmail.com>
2021-04-14 10:35:12 +0300

    learning react-query

--- src/App.js
+++ src/App.js
@@ -1,39 +1,33 @@
-// import logo from './logo.svg';
-// import './App.css';
+import React from 'react';
+import { useQuery } from 'react-query';
+
+const getPosts = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+  return response.json()
+};
 
 function App() {
+  const { status, data, isFetching, error } = useQuery('posts', getPosts);
+
+  if (status === 'loading') {
+    return <div>loading...</div> // loading state
+  }
+
+  if (status === 'error') {
+    return <div>{error.message}</div> // error state
+  }
+
   return (
-    <div className="max-w-4xl mx-auto">
-      <h1 className="text-2xl -font-bold mt-8 mb-5">Just once I'd like to eat dinner with a celebrity who isn't bound and gagged.</h1>
-      <p>I am Singing Wind, Chief of the Martians. I feel like I was mauled by Jesus. We don't have a brig. Oh God, what have I done?</p>
-      <p>Fatal. Why not indeed! Bender, I didn't know you liked cooking. That's so cute. Aww, it's true. I've been hiding it for so long. <strong> Hi, I'm a naughty nurse, and I really need someone to talk to.</strong> <em> $9.</em> 95 a minute.</p>
-      <h2>Why would a robot need to drink?</h2>
-      <p>Goodbye, friends. I never thought I'd die like this. But I always really hoped. Oh, how I wish I could believe or understand that! There's only one reasonable course of action now: kill Flexo! Oh yeah, good luck with that.</p>
-      <ol>
-      <li>It must be wonderful.</li><li>Okay, it's 500 dollars, you have no choice of carrier, the battery can't hold the charge and the reception isn't very…</li><li>Ooh, name it after me!</li>
-      </ol>
-
-      <button className="text-white font-bold bg-purple-700 hover:bg-purple-800 py-2 px-4 rounded">Party with Slurm</button>
-
-      <h3>Five hours? Aw, man! Couldn't you just get me the death penalty?</h3>
-      <p>Actually, that's still true. Is the Space Pope reptilian!? Man, I'm sore all over. I feel like I just went ten rounds with mighty Thor. I usually try to keep my sadness pent up inside where it can fester quietly as a mental illness.</p>
-      <ul>
-      <li>Look, everyone wants to be like Germany, but do we really have the pure strength of 'will'?</li><li>Ven ve voke up, ve had zese wodies.</li><li>Michelle, I don't regret this, but I both rue and lament it.</li>
-      </ul>
-
-      <p>Leela, are you alright? You got wanged on the head. Can we have Bender Burgers again? Fry, we have a crate to deliver. And from now on you're all named Bender Jr. Bender, quit destroying the universe!</p>
-      <p>Have you ever tried just turning off the TV, sitting down with your children, and hitting them? Yeah, and if you were the pope they'd be all, "Straighten your pope hat." And "Put on your good vestments."</p>
-      <p>Now Fry, it's been a few years since medical school, so remind me. Disemboweling in your species: fatal or non-fatal? Calculon is gonna kill us and it's all everybody else's fault! Why am I sticky and naked? Did I miss something fun?</p>
-      <p>No, of course not. It was… uh… porno. Yeah, that's it. Fry, we have a crate to deliver. For the last time, I don't like lilacs! Your 'first' wife was the one who liked lilacs! Say it in Russian!</p>
-      <p>Now what? Well, thanks to the Internet, I'm now bored with sex. Is there a place on the web that panders to my lust for violence? Yes, I saw. You were doing well, until everyone died. I guess if you want children beaten, you have to do it yourself.</p>
-      <p>I'm just glad my fat, ugly mama isn't alive to see this day. Morbo can't understand his teleprompter because he forgot how you say that letter that's shaped like a man wearing a hat. Why not indeed! For the last time, I don't like lilacs! Your 'first' wife was the one who liked lilacs!</p>
-      <p>Daylight and everything. Oh, I think we should just stay friends. Ah, yes! John Quincy Adding Machine. He struck a chord with the voters when he pledged not to go on a killing spree. And I'd do it again! And perhaps a third time! But that would be it.</p>
-      <p>You know, I was God once. Oh yeah, good luck with that. And why did 'I' have to take a cab? You are the last hope of the universe.</p>
-      <p>Can I use the gun? Well, let's just dump it in the sewer and say we delivered it. I meant 'physically'. Look, perhaps you could let me work for a little food? I could clean the floors or paint a fence, or service you sexually?</p>
-      <p>Oh God, what have I done? Oh dear! She's stuck in an infinite loop, and he's an idiot! Well, that's love for you. Oh Leela! You're the only person I could turn to; you're the only person who ever loved me.</p>
-      <p>You don't know how to do any of those. Good news, everyone! I've taught the toaster to feel love! Ask her how her day was. Or a guy who burns down a bar for the insurance money! Tell her you just want to talk. It has nothing to do with mating.</p>
+    <div>
+      { data && <ul>{
+        data
+          .slice(0,10) // only take frist 10 for now
+          // render list of titles
+          .map(d => <li key={`post-${d.id}`}>{d.title}</li>)
+      }</ul> }
+      { isFetching && <p>updating...</p> }
     </div>
-  );
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { ReactQueryDevtools } from 'react-query-devtools';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
